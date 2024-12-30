3b3f626f2b42302fdb3201eec8bc93e4e0851e67
Mariia_Ostapets <mariyaostapets@gmail.com>
2024-12-30 19:01:24 +0200

    Add functionality for adding photo

--- src/routers/students.js
+++ src/routers/students.js
@@ -17,6 +17,7 @@ import { isValidId } from '../middlewares/isValidId.js';
 import { authenticate } from '../middlewares/authenticate.js';
 import { ROLES } from '../constants/index.js';
 import { checkRoles } from '../middlewares/checkRoles.js';
+import { upload } from '../middlewares/multer.js';
 
 const router = Router();
 
@@ -32,8 +33,10 @@ router.get(
 );
 
 router.post(
-  '/register',
+  '/',
   checkRoles(ROLES.TEACHER),
+  isValidId,
+  upload.single('photo'),
   validateBody(createStudentSchema),
   ctrlWrapper(createStudentController),
 );
@@ -49,6 +52,7 @@ router.put(
   '/:studentId',
   checkRoles(ROLES.TEACHER),
   isValidId,
+  upload.single('photo'),
   validateBody(createStudentSchema),
   ctrlWrapper(upsertStudentController),
 );
@@ -57,6 +61,7 @@ router.patch(
   '/:studentId',
   checkRoles(ROLES.TEACHER, ROLES.PARENT),
   isValidId,
+  upload.single('photo'),
   validateBody(updateStudentSchema),
   ctrlWrapper(patchStudentController),
 );
